f41236f76c40f9c7ca51e3bbe7b07137bfc566d3
David Bell <dave.bell@me.com>
2021-01-21 13:34:10 +1100

    created package

--- Package.swift
+++ Package.swift
@@ -3,3 +3,26 @@ import PackageDescription
 let package = Package(
   name: "AudioPlayer"
 )
+
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AudioPlayer",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "AudioPlayer", targets: ["AudioPlayer"]),
+    ],
+    targets: [
+        .target(
+            name: "AudioPlayer",
+            path: "Sources"
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "AudioPlayer", path: "Binary/SVProgressHUD.xcframework"),
+    ]
+)
